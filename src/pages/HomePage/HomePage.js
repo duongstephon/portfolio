208790e10c4a8aed354aeb0882dc0c4bca8e132a
stephonduong <stephonduong@gmail.com>
2022-08-30 12:37:54 -0400

    added profile picture

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './HomePage.scss';
+import ProfilePic from '../../assets/images/profile-pic.png'
 
 const HomePage = () => {
   return (
@@ -9,7 +10,7 @@ const HomePage = () => {
           <h1 className='homepage__title'>Stephon Duong</h1>
           <p className='homepage__description'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum in ex labore quo. Minus, fugiat sapiente aliquid veritatis dicta beatae animi neque unde quisquam ex similique voluptate pariatur cupiditate amet. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis ex illo expedita, provident blanditiis dicta iste esse dolores incidunt. Nobis, alias quasi suscipit fugit ipsam delectus commodi nisi voluptatibus rerum.</p>
         </div>
-
+        <img className='homepage__image' src={ProfilePic} alt='profile picture' />
       </section>
 
     </div>
