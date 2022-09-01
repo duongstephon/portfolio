f26f41765b6b0097f1cd2a41cc9981332144dca1
stephonduong <stephonduong@gmail.com>
2022-09-01 17:02:50 -0400

    added emailjs

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Navbar.scss';
 import logo from '../../assets/images/sdlogo.png';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <Link to='/'><img className='navbar__logo' src={logo} alt='temp' /></Link>
+      <NavLink to='/'><img className='navbar__logo' src={logo} alt='temp' /></NavLink>
       <div className='navbar__navlinks'>
-        <Link to='/' className='navbar__link'><p>Home</p></Link>
-        <Link to='/projects' className='navbar__link'><p>Projects</p></Link>
-        <Link to='/contact' className='navbar__link'><p>Contact</p></Link>
+        <NavLink to='/' className='navbar__link'><p>Home</p></NavLink>
+        <NavLink to='/projects' className='navbar__link'><p>Projects</p></NavLink>
+        <NavLink to='/contact' className='navbar__link'><p>Contact</p></NavLink>
       </div>
     </div>
   );
