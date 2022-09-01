2af7f93ef108bcbc9bc7bead3bd932584d42d9e9
stephonduong <stephonduong@gmail.com>
2022-09-01 10:48:23 -0400

    added 2 new pages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import './App.scss';
 import Navbar from './components/Navbar/Navbar';
 import HomePage from './pages/HomePage/HomePage';
 import Footer from './components/Footer/Footer';
-import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className='app'>
+    <Router className='app'>
     <Navbar />
-    <HomePage />
+    <Switch>
+      <Route path='/' exact component={HomePage}/>
+      <Route path='/projects'component={HomePage}/>
+    </Switch>
     <Footer />
-    </div>
+    </Router>
   );
 }
 
