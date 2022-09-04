034a51290e07f17cb3bded33eec12dab380a6a62
stephonduong <stephonduong@gmail.com>
2022-09-04 15:43:07 -0400

    added modal and modal content to project page

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import './Modal.scss'
+import Readme from '../../assets/images/readme.png'
+import Instock from '../../assets/images/instock.png'
+import Brainflix from '../../assets/images/brainflix.png'
+import IndustryProject from '../../assets/images/industry.png'
 
-const Modal = ({ isShowing, hide }) => isShowing ? ReactDOM.createPortal(
-  <React.Fragment>
-    <div className="modal-overlay"/>
-    <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
-      <div className="modal">
-        <div className="modal-header">
-          <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <p>
-          Hello, I'm a modal.
-        </p>
-      </div>
+const projectImages = {'Read.ME': Readme, 'Google Industry Project': IndustryProject, 'InStock': Instock, 'Brainflix': Brainflix }
+
+const Modal = (props) => {
+  if (!props.showing) return null
+
+  return (
+    <div className='modal'>
+    <img className='modal__image' src={projectImages[props.project.title]} alt='project thumbnail' />
+    <h3>{props.project.title}</h3>
+    <p>{props.project.description}</p>
+    <button onClick={props.closeModal}>Close</button>
     </div>
-  </React.Fragment>, document.body
-) : null;
+  )
+}
 
 export default Modal;
