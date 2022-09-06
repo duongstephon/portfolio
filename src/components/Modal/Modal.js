444e3bb46512228641953c217589ba54d44110ff
stephonduong <stephonduong@gmail.com>
2022-09-06 12:13:34 -0400

    added project information

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Modal.scss'
-import Readme from '../../assets/images/readme.png'
-import Instock from '../../assets/images/instock.png'
-import Brainflix from '../../assets/images/brainflix.png'
-import IndustryProject from '../../assets/images/industry.png'
+import './Modal.scss';
+import Readme from '../../assets/images/readme.png';
+import Instock from '../../assets/images/instock.png';
+import Brainflix from '../../assets/images/brainflix.png';
+import IndustryProject from '../../assets/images/industry.png';
 
 const projectImages = {'Read.ME': Readme, 'Google Industry Project': IndustryProject, 'InStock': Instock, 'Brainflix': Brainflix }
 
@@ -12,10 +12,11 @@ const Modal = (props) => {
 
   return (
     <div className='modal'>
+    <button onClick={props.closeModal}>Close</button>
     <img className='modal__image' src={projectImages[props.project.title]} alt='project thumbnail' />
     <h3>{props.project.title}</h3>
     <p>{props.project.description}</p>
-    <button onClick={props.closeModal}>Close</button>
+    <a href={props.project.clientgit}><button>Client</button></a>
     </div>
   )
 }
