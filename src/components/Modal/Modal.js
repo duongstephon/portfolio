1edf905b963dee3d5d6a31d409550727d17d3cd0
stephonduong <stephonduong@gmail.com>
2022-09-07 15:10:55 -0400

    fixed modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,28 +5,31 @@ import Instock from '../../assets/images/instock.png';
 import Brainflix from '../../assets/images/brainflix.png';
 import IndustryProject from '../../assets/images/industry.png';
 
-const projectImages = {'Read.ME': Readme, 'Google Industry Project': IndustryProject, 'InStock': Instock, 'Brainflix': Brainflix }
+const projectImages = { 'Read.ME': Readme, 'Google Industry Project': IndustryProject, 'InStock': Instock, 'Brainflix': Brainflix }
 
 const Modal = (props) => {
   if (!props.showing) return null
 
   return (
     <div className='modal'>
-    <button onClick={props.closeModal}>Close</button>
-    <div className='modal__card'>
-      <img className='modal__image' src={projectImages[props.project.title]} alt='project thumbnail' />
-      <div className='modal__info'>
-        <h3 className='modal__title'>{props.project.title}</h3>
-        <p className='modal__description'>{props.project.description}</p>
-        <ul className='modal__techstack'>
-          {props.project.techstack.map(tech => {
-            return(
-              <li className='modal__techstack-tech'>{tech}</li>
-              )})}
-        </ul>
-        <a href={props.project.clientgit}><button className='modal__git-link'>Git Repository</button></a>
+    <div className='modal__wrapper' aria-modal aria-hidden tabIndex={-1} role="dialog">
+      <div className='modal__card'>
+        <button onClick={props.closeModal}>Close</button>
+        <img className='modal__image' src={projectImages[props.project.title]} alt='project thumbnail' />
+        <div className='modal__info'>
+          <h3 className='modal__title'>{props.project.title}</h3>
+          <p className='modal__description'>{props.project.description}</p>
+          <ul className='modal__techstack'>
+            {props.project.techstack.map(tech => {
+              return (
+                <li className='modal__techstack-tech'>{tech}</li>
+              )
+            })}
+          </ul>
+          <a href={props.project.clientgit}><button className='modal__git-link'>Git Repository</button></a>
+        </div>
       </div>
-    </div>
+      </div>
     </div>
   )
 }
