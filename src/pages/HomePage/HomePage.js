1a2862731017379e51d1832fc4f8b54fc9b3e5c1
stephonduong <stephonduong@gmail.com>
2022-09-07 12:48:01 -0400

    fixed css naming

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -10,22 +10,6 @@ const HomePage = () => {
         <div className='homepage__description'>
           <h1 className='homepage__title'>Stephon Duong</h1>
           <p className='homepage__text'>Hi, my name is Stephon Duong and I am a <span className='bolded'>Full-Stack Web Developer</span>. I enjoy problem solving and gaining knowledge in all things tech related. Check out some of the recent projects that I have worked on.</p>
-          {/*<div className='homepage__list'>
-            <ul>
-              <li>HTML/CSS</li>
-              <li>Sass</li>
-              <li>JavaScript</li>
-              <li>React.js</li>
-              <li>Axios</li>
-            </ul>
-            <ul>
-              <li>Python</li>
-              <li>Node.js</li>
-              <li>Express.js</li>
-              <li>Knex.js</li>
-              <li>MySQL</li>
-            </ul>
-  </div> */}
         </div>
         <img className='homepage__image' src={ProfilePic} alt='profile picture' />
       </section>
